26a74c9b6435f67f2b09b9e185992fa675fe2243
Kirari Kirisaki <kirisaki124@yahoo.com>
2018-08-30 21:28:05 +0700

    up

--- src/components/members-page/MembersDesktop.jsx
+++ src/components/members-page/MembersDesktop.jsx
@@ -1,89 +1,14 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import background_01 from '../../img/background-01.png';
-
-import img1 from '../../img/organ1.jpg';
-import img2 from '../../img/organ2.jpg';
-import img3 from '../../img/organ3.jpg';
-import img4 from '../../img/organ4.jpg';
-import img5 from '../../img/organ5.jpg';
-import img6 from '../../img/organ6.jpg';
 
 class MembersDesktop extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isBackgroundScroll: false
-    };
-    // phải bind nó
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    const bodyRect = document.body.getBoundingClientRect(),
-      elemRect = document.getElementById('whoweare-intro-desktop').getBoundingClientRect(),
-      offset = elemRect.top - bodyRect.top;
-    window.scrollTo(0, offset - 96);
-    this.props.switchPage();
-  }
-
+  
   render() {
-    const { i18n } = this.props;
-
     return (
       <div>
-        <img style={{ width: '100%' }} src={background_01} alt="background_01" />
-        <div className="abt-wrap">
-          <div id="whoweare-intro-desktop" className="whoweare">
-            {i18n.organizationUpper}
-          </div>
-
-          <div className="person-wrap">
-            <img className="abt-img-organ" src={img1} alt="img1" />
-            <div className="abt-detail-organ" style={{ fontWeight: '500' }}>
-              {i18n.aboutDetail01DesktopOrganization}
-            </div>
-          </div>
-
-          <div className="person-wrap">
-            <div className="abt-detail-organ">{i18n.aboutDetail02DesktopOrganization}</div>
-            <img className="abt-img-organ" src={img2} alt="img2" />
-          </div>
-
-          <div className="person-wrap">
-            <img className="abt-img-organ" src={img3} alt="img3" />
-            <div className="abt-detail-organ">{i18n.aboutDetail03DesktopOrganization}</div>
-          </div>
-
-          <div className="person-wrap">
-            <div className="abt-detail-organ">{i18n.aboutDetail04DesktopOrganization}</div>
-            <img className="abt-img-organ" src={img4} alt="img4" />
-          </div>
-
-          <div className="person-wrap">
-            <img className="abt-img-organ" src={img5} alt="img5" />
-            <div className="abt-detail-organ">{i18n.aboutDetail05DesktopOrganization}</div>
-          </div>
-
-          <div className="person-wrap" style={{ marginBottom: '5%' }}>
-            <div className="abt-detail-organ">{i18n.aboutDetail06DesktopOrganization}</div>
-            <img className="abt-img-organ" src={img6} alt="img6" />
-          </div>
-        </div>
+      a
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  const { i18n } = state;
-  return {
-    i18n
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  null
-)(MembersDesktop);
+export default MembersDesktop;
--- src/components/members-page/MembersMobile.jsx
+++ src/components/members-page/MembersMobile.jsx
@@ -1,92 +1,14 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import ScrollHint from '../about-page/ScrollHint';
-import backgroundMobile_01 from '../../img/background-01-mobile.png';
-
-import img1 from '../../img/organ1.jpg';
-import img2 from '../../img/organ2.jpg';
-import img3 from '../../img/organ3.jpg';
-import img4 from '../../img/organ4.jpg';
-import img5 from '../../img/organ5.jpg';
-import img6 from '../../img/organ6.jpg';
 
 class MembersMobile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isBackgroundScroll: false
-    };
-    // phải bind nó
-    this.handleBackgroundScroll = this.handleBackgroundScroll.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleBackgroundScroll() {
-    const div = document.getElementById('scrollcenter-organ');
-    if (div && div.scrollLeft > 200) this.setState({ isBackgroundScroll: true });
-  }
-
-  handleClick() {
-    const bodyRect = document.body.getBoundingClientRect(),
-      elemRect = document.getElementById('whoweare-mb-organ').getBoundingClientRect(),
-      offset = elemRect.top - bodyRect.top;
-    window.scrollTo(0, offset - 48);
-    this.props.switchPage();
-  }
-
+  
   render() {
-    const { i18n } = this.props;
-
     return (
       <div>
-        <div className="bg-mobile-clb" id="scrollcenter-organ" onScroll={this.handleBackgroundScroll}>
-          <img className="bg-mobile-clb-img" src={backgroundMobile_01} alt="backgroundMobile_01" />
-          {this.state.isBackgroundScroll === false && <ScrollHint />}
-        </div>
-
-        <div className="abt-mb-wrap">
-          <div id="whoweare-mb-organ" className="whoweare">
-            {i18n.organizationUpper}
-          </div>
-          {/* <div className="abt-mb-avatar-wrap">
-          <img className="abt-mb-avatar" src={chairman} alt="chairman" />
-        </div> */}
-          <img className="abt-mb-img" src={img1} alt="img1" />
-          <div className="abt-mb-detail" style={{ fontWeight: '500' }}>
-            {i18n.aboutDetail01MobileOrganization}
-          </div>
-
-          <img className="abt-mb-img" src={img2} alt="img2" />
-          <div className="abt-mb-detail">{i18n.aboutDetail02MobileOrganization}</div>
-
-          <img className="abt-mb-img" src={img3} alt="img3" />
-          <div className="abt-mb-detail">{i18n.aboutDetail03MobileOrganization}</div>
-
-          <img className="abt-mb-img" src={img4} alt="img4" />
-          <div className="abt-mb-detail">{i18n.aboutDetail04MobileOrganization}</div>
-
-          <img className="abt-mb-img" src={img5} alt="img5" />
-          <div className="abt-mb-detail">{i18n.aboutDetail05MobileOrganization}</div>
-
-          <img className="abt-mb-img" src={img6} alt="img6" />
-          <div className="abt-mb-detail" style={{ marginBottom: '10%' }}>
-            {i18n.aboutDetail06MobileOrganization}
-          </div>
-        </div>
+      a
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  const { i18n } = state;
-  return {
-    i18n
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  null
-)(MembersMobile);
+export default MembersMobile;
